b6427fa867ea0be96e0370422fe078efd765f148
Ionatan Sala <sala.jonatan1995@gmail.com>
2015-12-02 20:25:12 +0000

    add gravatar, show articles and users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,8 @@
 class UsersController < ApplicationController
+	def index
+		@users = User.all
+	end
+
 	def new
 		@user = User.new
 	end
@@ -27,6 +31,11 @@ class UsersController < ApplicationController
 		else
 			render 'edit'
 		end
+	end
+
+	def show
+		@user = User.find(params[:id])
+		
 	end
 
 	private
