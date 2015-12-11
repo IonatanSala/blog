e28357bbb95eeebaff2f30c01c7556ffe4319e35
Ionatan Sala <sala.jonatan1995@gmail.com>
2015-12-11 14:44:21 +0000

    add ability to add category on creation of article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,7 +2,7 @@ class ArticlesController < ApplicationController
 	before_action :set_article, only: [:edit, :update, :show, :destroy]
 	before_action :require_user, except: [:index, :show]
 	before_action :require_same_user, only: [:edit, :update, :destroy]
-	
+
 	def index
 		@articles = Article.paginate(page: params[:page], per_page: 5)
 	end
@@ -22,7 +22,7 @@ class ArticlesController < ApplicationController
 		end
 	end
 
-	def show 
+	def show
 	end
 
 	def edit
@@ -49,13 +49,13 @@ class ArticlesController < ApplicationController
 	end
 
 	def article_params
-		params.require(:article).permit(:title, :description)
+		params.require(:article).permit(:title, :description, category_ids: [])
 	end
 
 	def require_same_user
 		if current_user != @article.user && !current_user.admin?
 			flash[:danger] = "You can only edit or delete your own articles"
 			redirect_to root_path
-		end	
+		end
 	end
 end
